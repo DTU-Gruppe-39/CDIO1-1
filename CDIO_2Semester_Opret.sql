f15bf1b427782218b710da667d05619da797874b
Rasmus Gregersen <raller799@live.dk>
2018-02-22 12:27:18 +0100

    Add files via upload

---
+++ CDIO_2Semester_Opret.sql
@@ -0,0 +1,8 @@
+CREATE TABLE personer (
+						userID int,
+						userName varchar(40), 
+                        ini varchar(6),
+                        roles varchar(10),
+                        cpr varchar(11),
+                        passwd varchar(64)
+                        );
---
+++ test_data_cdio.sql
@@ -0,0 +1,2 @@
+Insert into personer(userID, userName, ini, roles, cpr, passwd) values
+(1, 'test', 'tst', 'admin', '123456-8888', 'secure');
